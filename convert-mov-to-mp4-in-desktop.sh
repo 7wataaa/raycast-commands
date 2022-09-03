4281e42c914ae51d792f48f56900df7d383474e2
nashi <nn.contact.n@gmail.com>
2022-09-04 04:12:37 +0900

    [add] "Convert mov to mp4 in Desktop" command

---
+++ convert-mov-to-mp4-in-desktop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Convert mov to mp4 in Desktop
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🎬
+# @raycast.needsConfirmation true
+
+# Documentation:
+# @raycast.description Convert mov to mp4 in Desktop
+# @raycast.authorURL https://github.com/7wataaa
+
+cd "$HOME/Desktop" || exit 1
+
+FFMPEG_PATH="/usr/local/bin/ffmpeg"
+
+if [[ ! -e "$FFMPEG_PATH" ]]; then
+  echo "ERROR: $FFMPEG_PATH is not installed."
+  exit 1
+fi
+
+for file in *
+do
+  if [[ "$file" == *".mov" ]] && [[ ! -e "${file//.mov/.mp4}" ]]; then
+    echo "Converting ${file}..."
+
+    tmpOutputName="${file//.mov/.mp4}"
+    # Space characters need to be escaped
+    bash -c "$FFMPEG_PATH -i ${file// /\ } ${tmpOutputName// /\ }"
+  fi
+done
+
+wait
